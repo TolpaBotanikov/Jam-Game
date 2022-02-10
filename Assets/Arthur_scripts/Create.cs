45726974b37561577f3a61c98a497b4be0b8b90f
Murka325 <umeroffmurat@yandex.ru>
2022-02-10 15:35:05 +0300

    spok

---
+++ Assets/Arthur_scripts/Create.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Create : MonoBehaviour, IBeginDragHandler
+{
+    public GameObject tower;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Instantiate(tower, eventData.pointerCurrentRaycast.worldPosition, Quaternion.identity);
+    }
+}
