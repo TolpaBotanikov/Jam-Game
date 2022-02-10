98536f2121ce7957ddb3752dbec162e04bc43fc0
Artur <61884137+Dathator@users.noreply.github.com>
2022-02-10 21:57:27 +0300

    1234562345678

---
+++ Assets/Arthur_scripts/Tyrel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Tyrel : MonoBehaviour, IDragHandler, IBeginDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.pointerCurrentRaycast.worldPosition;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.pointerCurrentRaycast.worldPosition;
+    }
+}
