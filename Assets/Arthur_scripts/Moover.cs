e26882bb7b39e4ae55d466f133f7e85527408538
Murka325 <umeroffmurat@yandex.ru>
2022-02-10 17:45:42 +0300

    end

--- Assets/Arthur_scripts/Moover.cs
+++ Assets/Arthur_scripts/Moover.cs
@@ -3,25 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Moover : MonoBehaviour, IDragHandler
+public class Moover : MonoBehaviour
 {
-    public GameObject mesh;
-    public void OnDrag(PointerEventData eventData)
-    {
-        Instantiate(mesh, eventData.pointerCurrentRaycast.worldPosition, Quaternion.identity);
-        print(eventData.pointerCurrentRaycast.worldPosition);
-    }
-
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        for (int i = 0; i <= 25; i++)
+        float scalex = transform.localScale.x * 10;
+        float scalez = transform.localScale.z * 10;
+        for (int i = 0; i <= scalex; i++)
         {
-            Gizmos.DrawLine(new Vector3(-12.5f + i, 0.001f, 25), new Vector3(-12.5f + i, 0.001f, -25));
+            Gizmos.DrawLine(new Vector3(-scalex / 2 + i, 0.001f, scalez / 2), new Vector3(-scalex / 2 + i, 0.001f, -scalez / 2));
         }
-        for (int i = 0; i <= 50; i++)
+        for (int i = 0; i <= scalez; i++)
         {
-            Gizmos.DrawLine(new Vector3(-12.5f, 0.001f, 25 - i), new Vector3(12.5f, 0.001f, 25 - i));
+            Gizmos.DrawLine(new Vector3(-scalex / 2, 0.001f, (scalez / 2) - i), new Vector3(scalex / 2, 0.001f, (scalez / 2) - i));
         }
     }
 }
