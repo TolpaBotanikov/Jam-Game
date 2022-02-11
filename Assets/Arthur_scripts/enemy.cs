ef12b6f54a62823e4c7e89cd4c1c9e2f71c62f18
Murka325 <umeroffmurat@yandex.ru>
2022-02-11 23:36:16 +0300

    fiksik

---
+++ Assets/Arthur_scripts/enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public int HP;
+    public int DAMAGE;
+    public float SPEED;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position + GetComponent<SphereCollider>().center, GetComponent<SphereCollider>().radius);
+    }
+}
