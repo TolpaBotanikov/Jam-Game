4d3e9e4effd3fdc1ea476c6dfaaa3522d1077124
Murka325 <umeroffmurat@yandex.ru>
2022-02-11 13:12:26 +0300

    menuChange

---
+++ Assets/PauseBut.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseBut : MonoBehaviour
+{
+    public GameObject pause;
+    public void PauseGame()
+    {
+        pause.SetActive(true);
+        Time.timeScale = 0f;
+    }
+}
---
+++ Assets/TapStart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class TapStart : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Time.timeScale = 1f;
+    }
+}
